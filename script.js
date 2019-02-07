1cc003a4d19b8a25af8a699065f036634a251ae0
Georgescu Gabriel <29951139+gabi200@users.noreply.github.com>
2019-02-07 15:17:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+﻿
+// toggle menu status
+
+function toggle_view(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// check if new user
+
+if ( localStorage.newUser != 0 ) {
+      localStorage.newUser = 0;
+      var name = prompt("Hi! What's your name?");
+      localStorage.name = name;
+      alert("Welcome, " + name + " !");
+}
+
+// info popup
+
+var info_msg = "EasyCode™ v1.0.2\nBy Georgescu Gabriel\n©2019 Gabi Software.All rights reserved.";
+
+// open code editor
+
+function open_editor() {
+	window.open("../editor/code.html", "Code Editor", "width=650,height=430")
+}
