c274a9b6c672289ec0a45ce12637862ca111a5c1
Georgescu Gabriel <29951139+gabi200@users.noreply.github.com>
2019-07-31 13:04:46 +0300

    Fixed a bug with the navigation menu

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 
 function toggle_view(id) {
     var x = document.getElementById(id);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
 
 // check if new user
